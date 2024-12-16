c4418e5d68e78c72782d27d92b87e0cd5b9a861d
Halmen Istvan <isti@mobiscroll.com>
2024-12-16 15:07:27 +0200

    Sync demos from main repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -250,6 +250,7 @@ import { AppComponent as Demo750 } from '../demos/eventcalendar/timeline/tasks-s
 import { AppComponent as Demo751 } from '../demos/eventcalendar/timeline/resource-filtering-search/resource-filtering-search';
 import { AppComponent as Demo605 } from '../demos/eventcalendar/timeline/daily-weekly-monthly-yearly-timeline/daily-weekly-monthly-yearly-timeline';
 import { AppComponent as Demo663 } from '../demos/eventcalendar/timeline/multiple-days-weeks-months-quarters-years-variable-resolution/multiple-days-weeks-months-quarters-years-variable-resolution';
+import { AppComponent as Demo764 } from '../demos/eventcalendar/timeline/calendar-zoom/calendar-zoom';
 import { AppComponent as Demo734 } from '../demos/eventcalendar/timeline/control-number-of-concurrently-shown-events/control-number-of-concurrently-shown-events';
 import { AppComponent as Demo739 } from '../demos/eventcalendar/timeline/event-buffer/event-buffer';
 import { AppComponent as Demo697 } from '../demos/eventcalendar/timeline/custom-range-view/custom-range-view';
@@ -275,6 +276,8 @@ import { AppComponent as Demo728 } from '../demos/eventcalendar/timeline/prevent
 import { AppComponent as Demo607 } from '../demos/eventcalendar/timeline/timeline-resource-details-side-panel-footer/timeline-resource-details-side-panel-footer';
 import { AppComponent as Demo625 } from '../demos/eventcalendar/timeline/resource-grouping-hierarchy/resource-grouping-hierarchy';
 import { AppComponent as Demo747 } from '../demos/eventcalendar/timeline/show-summaries-aggregates-for-resource-groups/show-summaries-aggregates-for-resource-groups';
+import { AppComponent as Demo765 } from '../demos/eventcalendar/timeline/resource-drag-drop-reorder/resource-drag-drop-reorder';
+import { AppComponent as Demo768 } from '../demos/eventcalendar/timeline/dynamic-calculation-resource-sort/dynamic-calculation-resource-sort';
 import { AppComponent as Demo720 } from '../demos/eventcalendar/timeline/resource-data-structure/resource-data-structure';
 import { AppComponent as Demo666 } from '../demos/eventcalendar/timeline/event-data-structure/event-data-structure';
 import { AppComponent as Demo706 } from '../demos/eventcalendar/timeline/connecting-linking-events-arrows/connecting-linking-events-arrows';
@@ -283,7 +286,9 @@ import { AppComponent as Demo668 } from '../demos/eventcalendar/timeline/recurri
 import { AppComponent as Demo677 } from '../demos/eventcalendar/timeline/load-inline-data/load-inline-data';
 import { AppComponent as Demo669 } from '../demos/eventcalendar/timeline/load-events-from-remote-api/load-events-from-remote-api';
 import { AppComponent as Demo670 } from '../demos/eventcalendar/timeline/load-events-on-demand/load-events-on-demand';
+import { AppComponent as Demo766 } from '../demos/eventcalendar/timeline/load-events-on-scroll/load-events-on-scroll';
 import { AppComponent as Demo718 } from '../demos/eventcalendar/timeline/load-resources-on-demand/load-resources-on-demand';
+import { AppComponent as Demo767 } from '../demos/eventcalendar/timeline/load-resources-on-scroll/load-resources-on-scroll';
 import { AppComponent as Demo704 } from '../demos/eventcalendar/timeline/loading-big-data-sets/loading-big-data-sets';
 import { AppComponent as Demo684 } from '../demos/eventcalendar/timeline/sync-events-google-calendar/sync-events-google-calendar';
 import { AppComponent as Demo688 } from '../demos/eventcalendar/timeline/sync-events-outlook-calendar/sync-events-outlook-calendar';
@@ -983,6 +988,7 @@ export const demos: any[] = [
             items: [
               { name: 'Configure the timeline', unique: 'daily-weekly-monthly-yearly-timeline', component: Demo605 },
               { name: 'Configure the page, size, resolution', unique: 'multiple-days-weeks-months-quarters-years-variable-resolution', component: Demo663 },
+              { name: 'Dynamically zoom in/out', unique: 'calendar-zoom', component: Demo764 },
               { name: 'Set event stack size', unique: 'control-number-of-concurrently-shown-events', component: Demo734 },
               { name: 'Event buffer', unique: 'event-buffer', component: Demo739 },
               { name: 'View with custom range picker', unique: 'custom-range-view', component: Demo697 },
@@ -1026,6 +1032,8 @@ export const demos: any[] = [
               { name: 'Resource grid', unique: 'timeline-resource-details-side-panel-footer', component: Demo607 },
               { name: 'Resource grouping & hierarchy', unique: 'resource-grouping-hierarchy', component: Demo625 },
               { name: 'Resource group summaries', unique: 'show-summaries-aggregates-for-resource-groups', component: Demo747 },
+              { name: 'Resource reorder with d&d', unique: 'resource-drag-drop-reorder', component: Demo765 },
+              { name: 'Dynamic resource sort', unique: 'dynamic-calculation-resource-sort', component: Demo768 },
               { name: 'Resource properties', unique: 'resource-data-structure', component: Demo720 },
             ],
           },
@@ -1046,7 +1054,9 @@ export const demos: any[] = [
               { name: 'Loading inline data', unique: 'load-inline-data', component: Demo677 },
               { name: 'Events from remote API ', unique: 'load-events-from-remote-api', component: Demo669 },
               { name: 'Loading events on demand', unique: 'load-events-on-demand', component: Demo670 },
+              { name: 'Loading events on scroll', unique: 'load-events-on-scroll', component: Demo766 },
               { name: 'Loading resources on demand', unique: 'load-resources-on-demand', component: Demo718 },
+              { name: 'Loading resources on scroll', unique: 'load-resources-on-scroll', component: Demo767 },
               { name: 'Working with large data sets', unique: 'loading-big-data-sets', component: Demo704 },
             ],
           },
@@ -1510,6 +1520,7 @@ export const demoTitleMap: { [key: string]: string } = {};
     Demo751,
     Demo605,
     Demo663,
+    Demo764,
     Demo734,
     Demo739,
     Demo697,
@@ -1535,6 +1546,8 @@ export const demoTitleMap: { [key: string]: string } = {};
     Demo607,
     Demo625,
     Demo747,
+    Demo765,
+    Demo768,
     Demo720,
     Demo666,
     Demo706,
@@ -1543,7 +1556,9 @@ export const demoTitleMap: { [key: string]: string } = {};
     Demo677,
     Demo669,
     Demo670,
+    Demo766,
     Demo718,
+    Demo767,
     Demo704,
     Demo684,
     Demo688,
