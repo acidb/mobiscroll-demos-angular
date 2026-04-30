9d1cd82e22adff9fc8e4b5a38c3833c6f9789ddc
Halmen Istvan <isti@mobiscroll.com>
2026-05-01 00:48:59 +0300

    Sync demos from main repository

--- src/app/demos.ts
+++ src/app/demos.ts
@@ -177,6 +177,7 @@ import { AppComponent as Demo581 } from '../demos/eventcalendar/scheduler/work-w
 import { AppComponent as Demo721 } from '../demos/eventcalendar/scheduler/doctors-appointment/doctors-appointment';
 import { AppComponent as Demo786 } from '../demos/eventcalendar/scheduler/24-hour-manufacturing-shift-rota-planning/24-hour-manufacturing-shift-rota-planning';
 import { AppComponent as Demo769 } from '../demos/eventcalendar/scheduler/doctors-availability-calendar-planner/doctors-availability-calendar-planner';
+import { AppComponent as Demo791 } from '../demos/eventcalendar/scheduler/drop-assignee-on-event-from-list/drop-assignee-on-event-from-list';
 import { AppComponent as Demo696 } from '../demos/eventcalendar/scheduler/custom-range-view/custom-range-view';
 import { AppComponent as Demo733 } from '../demos/eventcalendar/scheduler/control-number-of-concurrently-shown-events/control-number-of-concurrently-shown-events';
 import { AppComponent as Demo664 } from '../demos/eventcalendar/scheduler/display-multiple-days-weeks/display-multiple-days-weeks';
@@ -217,6 +218,8 @@ import { AppComponent as Demo490 } from '../demos/eventcalendar/scheduler/recurr
 import { AppComponent as Demo517 } from '../demos/eventcalendar/scheduler/load-inline-data/load-inline-data';
 import { AppComponent as Demo483 } from '../demos/eventcalendar/scheduler/load-events-from-remote-api/load-events-from-remote-api';
 import { AppComponent as Demo482 } from '../demos/eventcalendar/scheduler/load-events-on-demand/load-events-on-demand';
+import { AppComponent as Demo788 } from '../demos/eventcalendar/scheduler/load-events-on-scroll/load-events-on-scroll';
+import { AppComponent as Demo789 } from '../demos/eventcalendar/scheduler/loading-big-data-sets/loading-big-data-sets';
 import { AppComponent as Demo682 } from '../demos/eventcalendar/scheduler/sync-events-google-calendar/sync-events-google-calendar';
 import { AppComponent as Demo687 } from '../demos/eventcalendar/scheduler/sync-events-outlook-calendar/sync-events-outlook-calendar';
 import { AppComponent as Demo500 } from '../demos/eventcalendar/scheduler/load-events-from-google-calendar/load-events-from-google-calendar';
@@ -253,6 +256,7 @@ import { AppComponent as Demo751 } from '../demos/eventcalendar/timeline/resourc
 import { AppComponent as Demo772 } from '../demos/eventcalendar/timeline/resource-management-with-dynamic-team-creation/resource-management-with-dynamic-team-creation';
 import { AppComponent as Demo785 } from '../demos/eventcalendar/timeline/event-grouping-bulk-action/event-grouping-bulk-action';
 import { AppComponent as Demo787 } from '../demos/eventcalendar/timeline/36-hour-rolling-window-aircraft-view/36-hour-rolling-window-aircraft-view';
+import { AppComponent as Demo790 } from '../demos/eventcalendar/timeline/drop-assignee-on-event-from-list/drop-assignee-on-event-from-list';
 import { AppComponent as Demo605 } from '../demos/eventcalendar/timeline/daily-weekly-monthly-yearly-timeline/daily-weekly-monthly-yearly-timeline';
 import { AppComponent as Demo663 } from '../demos/eventcalendar/timeline/multiple-days-weeks-months-quarters-years-variable-resolution/multiple-days-weeks-months-quarters-years-variable-resolution';
 import { AppComponent as Demo764 } from '../demos/eventcalendar/timeline/calendar-zoom/calendar-zoom';
@@ -836,6 +840,7 @@ export const demos: any[] = [
               { name: 'Doctor’s appointment', unique: 'doctors-appointment', component: Demo721 },
               { name: 'Continuous shift planning', unique: '24-hour-manufacturing-shift-rota-planning', component: Demo786 },
               { name: 'Availability planner & appointment manager', unique: 'doctors-availability-calendar-planner', component: Demo769 },
+              { name: 'Event drop assignment', unique: 'drop-assignee-on-event-from-list', component: Demo791 },
             ],
           },
           {
@@ -912,6 +917,8 @@ export const demos: any[] = [
               { name: 'Loading inline data', unique: 'load-inline-data', component: Demo517 },
               { name: 'Events from remote API', unique: 'load-events-from-remote-api', component: Demo483 },
               { name: 'Loading events on demand', unique: 'load-events-on-demand', component: Demo482 },
+              { name: 'Loading events on scroll', unique: 'load-events-on-scroll', component: Demo788 },
+              { name: 'Working with large data sets', unique: 'loading-big-data-sets', component: Demo789 },
             ],
           },
           {
@@ -990,6 +997,7 @@ export const demos: any[] = [
               { name: 'Set up custom teams', unique: 'resource-management-with-dynamic-team-creation', component: Demo772 },
               { name: 'Group events by client and apply custom actions', unique: 'event-grouping-bulk-action', component: Demo785 },
               { name: '36 hour rolling view', unique: '36-hour-rolling-window-aircraft-view', component: Demo787 },
+              { name: 'Event drop assignment', unique: 'drop-assignee-on-event-from-list', component: Demo790 },
             ],
           },
           {
